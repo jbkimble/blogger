337a7db327893b99fd1f70a3e3c2f46c9881d4b8
Jon Kimble <jonathanbkimble@gmail.com>
2016-10-16 19:12:09 -0600

    Tagging Feature done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    flash.notice = "Article '#{@article.title} Deleted!'"
+    flash.notice = "Article '#{@article.title}' Deleted!"
 
     redirect_to articles_path
   end
